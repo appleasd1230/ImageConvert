35f29a7fbdda5115f5484781d2aa80c81ff43db6
Josh1994 <appleasd1230@gms.tku.edu.tw>
2022-08-03 15:45:36 +0800

    Update ImageConvert.py

--- ImageConvert.py
+++ ImageConvert.py
@@ -7,5 +7,23 @@
 # you can also take a look at other portfolios I made.
 # Visit https://github.com/appleasd1230.
 
+import cv2
+import numpy as np
 
-def 
+def read_bytes_image(image: bytes) -> numpy.ndarray:
+  """Read image as byte type, most use in webAPI.
+  
+     if byte from request
+     image = request.files['file'].read()
+
+     if byte from parameter like below
+     image = b'\xff\xd8\xff\xe0\x00\x10JFIF\x00\x01\x01\x00\x00...........'
+  """
+  
+  # read bytes by using openCV
+  img = cv2.imdecode(np.frombuffer(image, np.uint8), cv2.IMREAD_COLOR)
+  ## if you also want to conver it to gray
+  # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  return img
+
+
