2eb26325d331070d5044909d53afb368e6c1ba9b
Josh1994 <appleasd1230@gms.tku.edu.tw>
2022-08-03 15:54:41 +0800

    Update ImageConvert.py

--- ImageConvert.py
+++ ImageConvert.py
@@ -26,4 +26,47 @@ def read_bytes_image(image: bytes) -> numpy.ndarray:
   # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
   return img
 
+def read_whihe_and_black_image(image: bytes) -> numpy.ndarray:
+    """Read 1 (1-bit pixels, black and white, stored with one pixel per byte) by openCV.
+       using PIL read image first,
+       if image mode is '1', conver it to mode 'L',
+       than opencv can read the mode 'L' bytes.
+       modes :
+       · 1 (1-bit pixels, black and white, stored with one pixel per byte)
+       · L (8-bit pixels, black and white)
+       · P (8-bit pixels, mapped to any other mode using a colour palette)
+       · RGB (3x8-bit pixels, true colour)
+       · RGBA (4x8-bit pixels, true colour with transparency mask)
+       · CMYK (4x8-bit pixels, colour separation)
+       · YCbCr (3x8-bit pixels, colour video format)
+       · I (32-bit signed integer pixels)
+       · F (32-bit floating point pixels)
 
+       if byte from request
+       image = request.files['file'].read()
+
+       if byte from parameter like below
+       image = b'\xff\xd8\xff\xe0\x00\x10JFIF\x00\x01\x01\x00\x00...........'
+    """
+    from PIL import Image
+    import io
+    
+    stream = io.BytesIO(image)
+    pil_img = Image.open(stream)
+
+    if pil_img.mode == '1':
+        pil_img.convert('L')
+        buf = io.BytesIO()
+        pil_img.save(buf, format='JPEG')
+        image = buf.getvalue()
+
+    stream.close()
+
+    img = cv2.imdecode(np.frombuffer(image, np.uint8), cv2.IMREAD_COLOR)
+    ## if you also want to conver it to gray
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return img
+  
+ 
+    
+    
