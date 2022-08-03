b6df707184ee03ad64373eba9d7c42f80a7feaad
Josh1994 <appleasd1230@gms.tku.edu.tw>
2022-08-03 15:19:32 +0800

    Update ImageConvert.py

--- ImageConvert.py
+++ ImageConvert.py
@@ -1,1 +1,11 @@
+# Copyright (c) 2022 Josh Hung. All Rights Reserved.
+#
+# You can use the all methods I offer for free.
+# You also can leave your options about my code,
+# we can have a discussion.
+# If the methods I provide can help you,
+# you can also take a look at other portfolios I made.
+# Visit https://github.com/appleasd1230.
 
+
+def 
